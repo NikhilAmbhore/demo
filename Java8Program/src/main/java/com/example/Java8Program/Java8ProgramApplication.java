c179b2f6028ab05e15711ab156b9786322bd5596
nikhilambhore <nikhil.ambhore@lonartech.com>
2021-10-01 19:12:06 +0530

    f

---
+++ Java8Program/src/main/java/com/example/Java8Program/Java8ProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java8Program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8ProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8ProgramApplication.class, args);
+	}
+
+}
